648c141a8be52d72f3d1439b7dbc67b0c374463b
Guillermo <guillerandreav@gmail.com>
2017-09-26 16:27:19 -0600

    index: create only the anothemr sentences
    registrar: create the conection, the another no have function

--- index.php
+++ index.php
@@ -11,9 +11,10 @@
 
 <form action="registrar.php" method="post">
     <label>User Name</label>
-    <input type="text" name="user_name"/>
-    <input type="submit" value="Submite"/>
-
+    <input type="text" name="user_name" id="user_name"/><br>
+    <br><label>Password</label>
+    <input type="text" name="password" id="pass"><br>
+    <br><input type="submit" value="Submite"/>
 
 </form>
 
--- registrar.php
+++ registrar.php
@@ -1,2 +1,22 @@
 <?php
-echo $_POST["user_name"];
+$mysqli= new mysqli("localhost","memo","HoffvjNzPBsQ4TZX","sat");
+//chech connection
+if ($mysqli->connect_errno){
+    die("Connect failed: ".$mysqli->connect_error);
+}
+//Insert user name and password
+/*$name= mysqli_real_escape_string($link,$_POST['name']);
+$password=mysqli_real_escape_string($link,$_POST['password']);
+*/
+//mysqli_query("INSERT INTO user(name,password) values ('Guillermo','123')");
+$sql="INSERT INTO USER (USERID,NAME, PASSWORD) VALUES (
+    '1',
+    '$_POST[name]',
+    '$_POST[password]',
+    )";
+
+/*if(mysqli_query($link, $sql)){
+    echo "Records added successfuly.";
+} else{
+    echo "ERROR: Could not able to execute $sql.".mysqli_error($link);
+}*/
