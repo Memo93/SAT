ea8316d51dcab2ddb0eaf5fb72a190627f429318
Guillermo <guillerandreav@gmail.com>
2017-09-26 17:09:59 -0600

    the same of index

--- registrar.php
+++ registrar.php
@@ -1,22 +1,14 @@
 <?php
-$mysqli= new mysqli("localhost","memo","HoffvjNzPBsQ4TZX","sat");
+$conexion= new mysqli("localhost","memo","HoffvjNzPBsQ4TZX","sat");
 //chech connection
-if ($mysqli->connect_errno){
-    die("Connect failed: ".$mysqli->connect_error);
+if ($conexion->connect_errno){
+    die("Connect failed: ".$conexion->connect_error);
 }
 //Insert user name and password
-/*$name= mysqli_real_escape_string($link,$_POST['name']);
-$password=mysqli_real_escape_string($link,$_POST['password']);
-*/
-//mysqli_query("INSERT INTO user(name,password) values ('Guillermo','123')");
-$sql="INSERT INTO USER (USERID,NAME, PASSWORD) VALUES (
-    '1',
-    '$_POST[name]',
-    '$_POST[password]',
-    )";
 
-/*if(mysqli_query($link, $sql)){
-    echo "Records added successfuly.";
-} else{
-    echo "ERROR: Could not able to execute $sql.".mysqli_error($link);
-}*/
+$name= (isset($_POST['user_name']) ? $_POST['user_name'] : '');
+$password = (isset($_POST['password']) ? $_POST['password'] : '');
+
+echo $name;
+echo $password;
+$query= "INSERT INTO user (name, password) VALUES ('$name','$password')";
