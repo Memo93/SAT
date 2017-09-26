d7ff03b303c68e8c49c6e658151cff144c7a6d30
Guillermo <guillerandreav@gmail.com>
2017-09-26 17:09:03 -0600

    cahnge the post, now i use isset

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title>Sistema de Administración de Tajos</title>
+    <link rel="stylesheet" type="text/css" href="style.css"
 </head>
 <body>
 <h1>
@@ -11,9 +12,9 @@
 
 <form action="registrar.php" method="post">
     <label>User Name</label>
-    <input type="text" name="user_name" id="user_name"/><br>
+    <input type="text" name="user_name"/><br>
     <br><label>Password</label>
-    <input type="text" name="password" id="pass"><br>
+    <input type="text" name="password"><br>
     <br><input type="submit" value="Submite"/>
 
 </form>
