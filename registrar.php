e2932182b61eb0c13d162cf88a89e1f59285ddcb
Guillermo <guillerandreav@gmail.com>
2017-09-26 21:06:49 -0600

    use of the object-oriented in the insert

--- registrar.php
+++ registrar.php
@@ -1,15 +1,15 @@
 <?php
-$conexion= new mysqli("localhost","memo","HoffvjNzPBsQ4TZX","sat");
+$mysqli= new mysqli("localhost","memo","HoffvjNzPBsQ4TZX","sat");
 //chech connection
-if ($conexion->connect_errno){
-    die("Connect failed: ".$conexion->connect_error);
+if ($mysqli->connect_errno){
+    die("Connect failed: ".$mysqli->connect_error);
 }
 
 //Insert user name and password
 $name= $_GET['username'];
 $password = $_GET['password'];
 
-$sql=mysqli_query($conexion,"INSERT INTO user (name,password) VALUES ('$name','$password')");
-$conexion->close();
+$sql= ($mysqli->query("INSERT INTO user (name,password) VALUES ('$name','$password')"));
+$mysqli->close();
 
 
