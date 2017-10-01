269cb5569655e3f614ac3b0c3d3d6dec772bd9f6
Guillermo <guillerandreav@gmail.com>
2017-10-01 14:03:02 -0600

    index
    create a simple menu

--- index.php
+++ index.php
@@ -2,43 +2,96 @@
 <head>
     <title>Sistema de Administración de Tajos</title>
     <style>
-        h1{
-            color:dodgerblue;
+
+        .container{
+            overflow: hidden;
+            background-color: black;
             font-family: "Times New Roman";
-            font-style: inherit;
-            font-size: 250%;
+        }
+        .container a {
+            float: left;
+            font-size: 16px;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+        .dropdown{
+            float: left;
+            overflow: hidden;
+        }
+        .dropdown .dropbtn {
+            cursor: pointer;
+            font-size: 16px;
+            border: none;
+            outline: none;
+            color: white;
+            padding: 14px 16px;
+            background-color: inherit;
         }
 
-        h2{
-            color: brown;
-            font-family: "Times New Roman";
-            font-style: inherit;
-            font-size: 100%;
+        .container a:hover, .dropdown:hover .dropbtn {
+            background-color: red;
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+        }
+
+        .dropdown-content a {
+            float: none;
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #ddd;
+        }
+
+        .show {
+            display: block;
         }
     </style>
 </head>
-<body>
-<h1>
-    <?php
-    echo "New User";
-    ?>
-</h1>
 
-<form action="access.php" method="get">
-    <h2>
-    <label>Name</label><br>
-    <input type="text"  placeholder="Name" name="name"/><br>
-    <br><label>Full Name</label><br>
-    <input type="text"  placeholder="Full Name" name="fullname"/><br>
-    <br><label>Email</label><br>
-    <input type="text"  placeholder="Email" name="email"/><br>
-    <br><label>User Name</label><br>
-    <input type="text" placeholder="User Name" name="username"><br>
-    <br><label>Password</label><br>
-    <input type="password" placeholder="Password" name="password"><br>
-    <br><input type="submit" value="Submite"/>
-    </h2>
+<div class="container">
+    <a href="index.php">Home</a>
+    <a href="create_user.php">Create User</a>
+    <div class="dropdown">
+        <button class="dropbtn" onclick="myFunction()">Crete new user</button>
+        <div class="dropdown-content" id="create_new_user">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+        </div>
+    </div>
+</div>
 
-</form>
-</body>
+<h3>Welcome to...</h3>
+
+<script>
+    /* When the user clicks on the button,
+     toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
+
+    // Close the dropdown if the user clicks outside of it
+    window.onclick = function(e) {
+            if (!e.target.matches('.dropbtn')) {
+                var myDropdown = document.getElementById("myDropdown");
+                if (myDropdown.classList.contains('show')) {
+                    myDropdown.classList.remove('show');
+                }
+            }
+        }
+</script>
 </html>
